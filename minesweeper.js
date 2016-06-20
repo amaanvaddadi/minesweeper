ad56178ed4e710463b2cb7053d25292663c55f21
amaanvaddadi <meetamaanrao@gmail.com>
2016-06-20 13:49:00 +1200

    added event listeners and show cell

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-function startGame () {
+function startGame ()  {
+    var children = document.getElementsByClassName('board')[0].children
+    console.log(children);
+    addListeners(children);
+    }
+
+function addListeners (element) {
+    for (var i = 0; i < element.length; i++) {
+        //console.log(elements[i])
+    element[i].addEventListener('click', showCell);
+    }
 }
+
+function showCell (evt) {
+    evt.target.classList.remove('hidden')
+}
+
+startGame()

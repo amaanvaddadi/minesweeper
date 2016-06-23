d1c7f53594d0d284417fe6710e8ef4892d3d5b61
amaanvaddadi <meetamaanrao@gmail.com>
2016-06-23 15:50:27 +1200

    Updated A lot of functions!

--- minesweeper.js
+++ minesweeper.js
@@ -1,53 +1,133 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-function startGame ()  {
+function startGame() {
     var children = document.getElementsByClassName('board')[0].children;
-    console.log(children);
-    addListeners(children);
-    }
+    for (var i = 0; i < children.length; i++) {
+        addListeners(children[i]);
+        addCellToBoard(children[i]);
+    } 
+    // console.log(board);
 
-function addListeners (element) {
-    for (var i = 0; i < element.length; i++) {
-        //console.log(elements[i])
-    element[i].addEventListener('click', showCell);
-    element[i].addEventListener('contextmenu', markCell);
-    }
+    for (var i = 0; i < board.cells.length; i++) {
+        var surroundingMines = countSurroundingMines(board.cells[i]);
+        board.cells[i].surroundingMines =  surroundingMines;
+     
+    } 
+} 
+
+function countSurroundingMines (cell) {
+    var surroundingCells =  getSurroundingCells(cell.row, cell.col);
+   
+    var numb = 0;
+    for (var i = 0; i < surroundingCells.length; i++) {
+        if (surroundingCells[i].isMine){ 
+            numb++;
+        }
+
+    } 
+    return numb;
 }
 
-function showCell (evt) {
+
+function addListeners(element) {
+    element.addEventListener('click', showCell);
+    element.addEventListener('contextmenu', markCell);
+}
+
+function showCell(evt) {
     evt.target.classList.remove('hidden');
+    if (evt.target.classList.contains('mine') ) {
+        showAllMines();
+        return 
+    }
+    showSurrounding(evt.target);
+    
+    checkForWin();
 }
 
-function markCell (evt) {
+function showAllMines () {
+       var mines = document.getElementsByClassName('mine');
+       for (var i=0; i<mines.length; i++){
+        mines[i].classList.remove('hidden');
+
+       }
+}
+function markCell(evt) {
 
     evt.preventDefault();
-
+    
     evt.target.classList.toggle('marked');
-}
-startGame();
+    evt.target.classList.toggle('hidden');
 
+    for (var i=0; i<board.cells.length; i++){
+        var row = getRow(evt.target);
+        var col = getCol(evt.target);
 
-var board  = {cells[]};
-var arr = str.split('-')
-
-function getRow (element){
-
-for (var i=0; i<elements.length; i++){
-
-    if (element(i).classList.contains('row- ')){return arr[1];}
-
+        if (board.cells[i].row === row && board.cells[i].col===col) { 
+            board.cells[i].isMarked = true; 
+        }
+    }
+    checkForWin();
 }
 
+function checkForWin () {
+console.log('test');
+    for (var i=0; i<board.cells.length; i++){
+        if (board.cells[i].isMine === true && board.cells[i].isMarked===false) { 
+            return  
+        }
+        var hidden = document.getElementsByClassName('hidden');
+        if (hidden.length > 0){
+
+            return
+        }
+
+        alert("You have won!");
+
+    }
 }
 
-function getCol (element){
+function addCellToBoard(element) {
 
-for (var i=0; i<elements.length; i++){
+    var newCell = {};
 
-    if (element(i).classList.contains('col- ')){return arr[1];}
+    newCell.row = getRow(element);
+    newCell.col = getCol(element);
+    newCell.isMine = element.classList.contains("mine");
+    board.cells.push(newCell);
+}
+
+var board = {
+    cells: []
+};
+
+function getRow(element) {
+    var elements = element.classList;
+
+    for (var i = 0; i < elements.length; i++) {
+
+        if (elements[i].indexOf("row-")> -1) {
+            
+               return parseInt(elements[i].split("row-").join(""));
+                      
+                }
+
+    }
 
 }
 
+function getCol(element) {
+ var elements = element.classList;
+    for (var i = 0; i < elements.length; i++) {
+
+        if (elements[i].indexOf("col-")> -1) {
+            return parseInt(elements[i].split("col-").join(""));
+
+        }
+
+    }
 }
+
+
 
 
--- minesweeper_lib.js
+++ minesweeper_lib.js
@@ -8,9 +8,6 @@ function getSurroundingCells (row, col) {
     .filter(function (cell) {
       return columns.includes(cell.col) && rows.includes(cell.row)
     })
-    .filter(function (cell) {
-      return !(cell.row === row && cell.col === col)
-    })
 }
 
 // For the given DOM element, displays surrounding mine counts
@@ -46,15 +43,15 @@ function setInnerHTML (cell) {
   if (element.classList.contains('marked')) {
     return
   }
+  element.innerHTML = cell.surroundingMines > 0 ?
+    cell.surroundingMines : ''
   if (element.classList.contains('hidden')) {
     element.classList.remove('hidden')
     if (cell.surroundingMines === 0) {
       return showSurrounding(element)
     }
-    element.innerHTML = cell.surroundingMines
   }
 }
-
 function getRange(begin, end) {
   return Array.apply(begin, Array(end - begin + 1))
     .map(function (n, i) {
